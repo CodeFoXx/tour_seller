ef679c4e31c8bdf48fc34cc4a000d3c686a3f5e3
YMaks <YelenaMaksimova@yandex.ru>
2016-09-29 15:13:10 +0700

    models fixed

--- consumers/models.py
+++ consumers/models.py
@@ -2,9 +2,18 @@ from django.db import models
 from tours.models import Tour
 
 
+class Booking(models.Model, list):
+    status = models.CharField(max_length=50)
+    startDate = models.DateTimeField()
+    finDate = models.DateTimeField()
+    bookedTours = models.ForeignKey(list(Tour))
+
+
 class Consumer(models.Model, list):
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=11)
-    tourCart = list()
+    tourCart = models.ForeignKey(list(Booking))
+
+
--- tours/models.py
+++ tours/models.py
@@ -4,18 +4,25 @@ from airlines.models import Airline
 from tourOperators.models import TourOperator
 from places.models import Hotel
 from places.models import City
+from consumers.models import Booking
 
 
 class Tour(models.Model):
     name = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    StartDate = models.DateField()
-    FinDate = models.DateField()
+    startDate = models.DateTimeField()
+    finDate = models.DateTimeField()
     airline = models.ForeignKey(Airline)
     tourOperator = models.ForeignKey(TourOperator)
     capacity = models.PositiveIntegerField()
     destination = models.ForeignKey(Hotel)
     departureCity = models.ForeignKey(City)
+
+
+def __str__(self):
+    return '{} ({:%Y-%m-%d} - {:%Y-%m-%d})'.format(
+        self.name, self.start, self.end)
+
 
 
 
