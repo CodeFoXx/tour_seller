c96730451eaf705eb30211c9b5a79bb2653ad9f1
Polina Osina <polinaosina14@gmail.com>
2016-09-30 02:06:35 +0700

    adding some files to empty rep

--- tourOperators/models.py
+++ tourOperators/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class TourOperator(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
--- tour_seller/urls.py
+++ tour_seller/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^tours/', include('tours.urls')),
+    url(r'^tourOperators/', include('tourOperators.urls')),
+    url(r'^places/', include('places.urls')),
+    url(r'^consumers/', include('consumers.urls')),
+    url(r'^airlines/', include('airlines.urls')),
 ]
