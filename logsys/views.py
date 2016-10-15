d3866d868c9e3f73aa0d99daa99020c985a4b28f
Polina Osina <polinaosina14@gmail.com>
2016-10-16 02:07:34 +0700

    new html file, migration etc

--- logsys/views.py
+++ logsys/views.py
@@ -15,6 +15,21 @@ def register(request):
     args['form'] = UserCreationForm()
 
     newuser_form = UserCreationForm(request.POST)
+    newuser_form.fields['username'].widget.attrs.update(
+        {
+            'placeholder':'name@example.com'
+        }
+    )
+    newuser_form.fields['password1'].widget.attrs.update(
+        {
+            'placeholder': 'password'
+        }
+    )
+    newuser_form.fields['password2'].widget.attrs.update(
+        {
+            'placeholder': 'password'
+        }
+    )
 
     if newuser_form.is_valid():
         user = Consumer.objects.create_consumer(email=newuser_form.cleaned_data['username'], password=newuser_form.cleaned_data['password2'])
