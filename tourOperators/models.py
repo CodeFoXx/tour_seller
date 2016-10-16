ee840e7f9e7fd0703a8ed18e5da398f0ce03e1f9
YMaks <YelenaMaksimova@yandex.ru>
2016-10-16 17:49:10 +0700

    tour_add method creation has been started

--- tourOperators/models.py
+++ tourOperators/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.forms import ModelForm
+from tours.models import Tour
 
 
 class TourOperator(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=11)
+    password = models.CharField(max_length=11, default="")
 
     def __str__(self):
-        return u'{}, {}, {}'.format(self.name,self.email,self.phone)
+        return u'{}, {}, {}'.format(self.name, self.email, self.phone)
+
+
+class AddTourForm(ModelForm):
+    class Meta:
+        model = Tour, TourOperator
+        fields = '__all__'
+        #exclude = ['title']
--- tourOperators/urls.py
+++ tourOperators/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-
 from tourOperators.views import TourOperListView
+from tourOperators.views import add_tour
 
 urlpatterns = [
-    url('^$', TourOperListView.as_view(), name='tourOp_list')
+    url('^$', TourOperListView.as_view(), name='tourOp_list'),
+    url('^$', add_tour),
 ]
